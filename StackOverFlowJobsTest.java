0946850a0e8385dabebbf3ab3d533c89f5b17e85
lmn33 <lmn33@pitt.edu>
2016-02-27 08:22:05 -0500

    Job tester

---
+++ StackOverFlowJobsTest.java
@@ -0,0 +1,113 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ * As a user,
+ * I want to be able to search companies and individual jobs,
+ * So that I can pick the best job for me
+ */
+
+@SuppressWarnings("unused")
+public class StackOverFlowJobsTest {
+
+	static WebDriver driver = new HtmlUnitDriver();
+	//starts at the main page
+	static WebDriver jobsSearch = new HtmlUnitDriver();
+	//starts at the jobs page
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		driver.get("http://stackoverflow.com/");
+		jobsSearch.get("http://stackoverflow.com/jobs");
+	}
+
+	// Given that I am on the main page
+	// When I view the title
+	// Then I see that it contains the word "Stack Overflow"
+	@Test
+	public void testShowsCorrectTitle() {
+		String title = driver.getTitle();
+		assertTrue(title.contains("Stack Overflow"));
+	}
+	
+	// Given that I am on the main page
+	// When I view the header
+	// Then I see that it contains "Jobs"
+	@Test
+	public void testHasCorrectHeaderLinks() 
+	{
+		try 
+		{
+			driver.findElement(By.linkText("Jobs"));
+		} 
+		catch (NoSuchElementException nseex) 
+		{
+			fail();
+		}
+	}
+	
+	//Given that the Jobs button is on the main page
+	//When I click on it
+	//Then I will be taken to another page titled "Job Listings - Stack Overflow"
+	@Test
+	public void testJobs()
+	{
+		try
+		{
+			driver.findElement(By.linkText("Jobs")).click();
+			String newPageTitle = driver.getTitle();
+			assertTrue(newPageTitle.contains("Job Listings - Stack Overflow"));
+		}
+		catch(NoSuchElementException nseex)
+		{
+			fail();
+		}
+	}
+	
+	//Given that there is a clickable Jobs button
+	//When click on it
+	//Then there should be a search jobs tab
+	@Test
+	public void testJobSearch()
+	{
+		try
+		{
+			driver.findElement(By.linkText("Jobs")).click();
+			//driver.findElement(By.linkText("new")).click();
+			jobsSearch.findElement(By.linkText("search jobs"));
+			String newPageTitle = driver.getTitle();
+			assertTrue(newPageTitle.contains("Job Listings - Stack Overflow"));
+		}
+		catch(NoSuchElementException nseex)
+		{
+			fail();
+		}
+	}
+	
+	//Given that there is a clickable Jobs button
+	//When click on it
+	//Then there should be a search companies tab
+	@Test
+	public void testCompanySearch()
+	{
+		try
+		{
+			driver.findElement(By.linkText("Jobs")).click();
+			//driver.findElement(By.linkText("new")).click();
+			jobsSearch.findElement(By.linkText("search companies"));
+			String newPageTitle = driver.getTitle();
+			assertTrue(newPageTitle.contains("Job Listings - Stack Overflow"));
+		}
+		catch(NoSuchElementException nseex)
+		{
+			fail();
+		}
+	}
+	
+}
